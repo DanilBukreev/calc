296d18620beb5d6bc8cbe4f9ced0d31ba241f9b0
DanilBukreev <31454061+DanilBukreev@users.noreply.github.com>
2017-09-29 11:32:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include<iostream>
 #include<stdio.h>
-using namespace std;
+using namespace std;    
+
 int main(int argc, char*argv[]) {
     cout << "count" << argc - 1 << endl;
-    for (int i = 1; i <=argc-1; i++){
-        cout << "argv[" << i << "]=" << argv[i] << endl;
+    for (int i = 1; i <= argc - 1; i++){
+       cout << "argv[" << i << "]=" << argv[i] << endl;
     }
+    
     char operand;
     operand = argv[2] [0];
-    switch (operand){
-    case '+':
+      switch (operand){
+        case '+':
         cout << atoi(argv[1]) + atoi(argv[3]) << endl;
         break;
-    case '-':
+        case '-':
         cout << atoi(argv[1]) - atoi(argv[3]) << endl;
         break;
-       default:
-        cerr << "error!!!!";
+        default:
+        cerr << " error!!!! ";
     }
 }
